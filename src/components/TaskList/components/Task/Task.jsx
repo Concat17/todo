ca41d57665e77b4ee5b63faed0d55369ecaaf9a3
Dmitriy Kurenkov <demel654@gmail.com>
2022-11-19 12:08:49 +0300

    refactoring

--- src/components/TaskList/components/Task/Task.jsx
+++ src/components/TaskList/components/Task/Task.jsx
@@ -5,26 +5,8 @@ import { Checkbox } from "../../../Checkbox";
 import { OpenButton } from "../../../OpenButton";
 import { IconButton } from "../../../IconButton";
 import { Button } from "../../../Button";
+import { AddFileIcon } from "../../../Icons";
 import "./Task.less";
-
-const AddFileIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      style={{ width: "1em", height: "1em" }}
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-      />
-    </svg>
-  );
-};
 
 export const Task = ({ title }) => {
   const [done, setDone] = useState(false);
@@ -38,12 +20,16 @@ export const Task = ({ title }) => {
           <Checkbox checked={done} onChange={setDone} />
         </div>
         {editing ? (
-          <input type="text" defaultValue={title} />
+          <input type="text" defaultValue={title} className="editable-field" />
         ) : (
           <span className={classNames({ strike: done })}>{title}</span>
         )}
         <div className="right">
-          <span>21.11.2022</span>
+          <input
+            type="date"
+            defaultValue="2022-07-22"
+            onChange={(e) => console.log(e.target.value)}
+          />
           <OpenButton open={open} onClick={() => setOpen((p) => !p)} />
         </div>
       </div>
@@ -54,6 +40,7 @@ export const Task = ({ title }) => {
         <div>
           <Button onClick={() => setEditing((p) => !p)}>Edit</Button>
           <Button>Save</Button>
+          <Button>Remove</Button>
         </div>
       </div>
     </li>
